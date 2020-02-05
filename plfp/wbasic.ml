bf01bfab215e8c7b918ff6138dba2415c688d340
iExploder <iexploder@hotmail.com>
2020-02-06 02:03:28 +0900

    updated assign/declare

--- plfp/wbasic.ml
+++ plfp/wbasic.ml
@@ -4,44 +4,66 @@ module WagonBasic = struct
     type wfxtype = wtype list * wtype
     type wexpr = wtype * string
     type wstmt = string
+    type 'a wlvalue = 'a
 
     (* Invisible Internal Functions for Users *)
-    let litmagic    : ('a -> string) -> wtype -> 'a -> wexpr =
+    let _litmagic    : ('a -> string) -> wtype -> 'a -> wexpr =
     fun serializer typ lit -> (typ, serializer lit)
-    let string_of_array : ('a -> string) -> string -> 'a array -> string =
+    let _string_of_array : ('a -> string) -> string -> 'a array -> string =
     fun serializer delimiter arr ->
         let fold_func s i = Printf.sprintf "%s%s%s" s delimiter @@ serializer i in
         let untrimmed_str = Array.fold_left fold_func "" arr in
         String.sub untrimmed_str (String.length delimiter) 
             ((String.length untrimmed_str) - (String.length delimiter))
 
-    let string_binop : string -> string -> string -> string =
+    let _string_binop : string -> string -> string -> string =
     fun op lhs rhs -> Printf.sprintf "(%s %s %s)" lhs op rhs
 
     let _binop        : (wtype -> wtype -> wtype) -> string -> wexpr -> wexpr -> wexpr =
-    fun typf op_str (t1, s1) (t2, s2) -> (typf t1 t2, string_binop op_str s1 s2)
+    fun typf op_str (t1, s1) (t2, s2) -> (typf t1 t2, _string_binop op_str s1 s2)
     let _typf_iii     : wtype -> wtype -> wtype =
     fun wt1 wt2 ->
     match (wt1, wt2) with
-    | (PDT(Int(l1)), PDT(Int(l2))) -> if l1 = l2 then wt1 
-                                        else failwith "unexpeccted bitlengths of integer operands"
+    | (PDT(Int(l1)), PDT(Int(l2))) -> 
+        if l1 = l2 then wt1 
+        else failwith "unexpeccted bitlengths of integer operands"
     | _ -> failwith "unexpected operand type(s)"
     let _typf_sss     : wtype -> wtype -> wtype =
     fun wt1 wt2 ->
     match (wt1, wt2) with
     | (PDT(FSingle), PDT(FSingle)) -> wt1
     | _ -> failwith "unexpected operand type(s)"
+
     let _typf_ddd     : wtype -> wtype -> wtype =
     fun wt1 wt2 ->
     match (wt1, wt2) with
     | (PDT(FDouble), PDT(FDouble)) -> wt1
     | _ -> failwith "unexpected operand type(s)"
 
+    let _namecounter  : int ref = ref (-1)
+    let _getname      : unit -> string = 
+    fun () -> 
+        let count = 
+            _namecounter := (!_namecounter) + 1; 
+            (!_namecounter) 
+        in
+        Printf.sprintf "wobj_%08x" count
+
+    let _declare_list : string list ref = ref []
+    let _declare_append : string -> unit =
+    fun str ->
+        let newlist = (!_declare_list) @ [str] in
+        _declare_list := newlist
+
+    let boollit     : bool -> wexpr =
+    fun b -> 
+        let bstr = if b then "true" else "false" in 
+        (PDT(Bool), bstr)
 
     let intlit      : wtype -> int -> wexpr =
     fun typ lit ->
         match typ with
-        | PDT(Int(i)) -> litmagic string_of_int typ lit
+        | PDT(Int(i)) -> _litmagic string_of_int typ lit
         | _ -> failwith "unexpected data type"
 
     let floatlit    : wtype -> float -> wexpr =
@@ -52,7 +74,7 @@ module WagonBasic = struct
                         | FSingle -> FSingle
                         | FDouble -> FDouble
                         | _ -> failwith "unexpected data type"
-                    in litmagic string_of_float (PDT(ftyp)) lit
+                    in _litmagic string_of_float (PDT(ftyp)) lit
         | _ -> failwith "unexpected data type"
 
     let vilit       : wtype -> int array -> wexpr =
@@ -65,7 +87,7 @@ module WagonBasic = struct
             | _ -> failwith "unexpected SIMD vector type"
         in
         if verify_len typ arr then 
-            (typ, Printf.sprintf "{%s}" @@ string_of_array string_of_int ", " arr)
+            (typ, Printf.sprintf "{%s}" @@ _string_of_array string_of_int ", " arr)
         else failwith "unexpected literal array length"
 
     let vslit       : wtype -> float array -> wexpr =
@@ -78,7 +100,7 @@ module WagonBasic = struct
             | _ -> failwith "unexpected SIMD vector type"
         in
         if verify_len typ arr then 
-            (typ, Printf.sprintf "{%s}" @@ string_of_array (Printf.sprintf "%ff") ", " arr)
+            (typ, Printf.sprintf "{%s}" @@ _string_of_array (Printf.sprintf "%ff") ", " arr)
         else failwith "unexpected literal array length"
 
     let vdlit       : wtype -> float array -> wexpr =
@@ -91,12 +113,47 @@ module WagonBasic = struct
             | _ -> failwith "unexpected SIMD vector type"
         in
         if verify_len typ arr then 
-            (typ, Printf.sprintf "{%s}" @@ string_of_array (Printf.sprintf "%f") ", " arr)
+            (typ, Printf.sprintf "{%s}" @@ _string_of_array (Printf.sprintf "%f") ", " arr)
         else failwith "unexpected literal array length"
     
     let addi        : wexpr -> wexpr -> wexpr = _binop _typf_iii "+"
+    let subi        : wexpr -> wexpr -> wexpr = _binop _typf_iii "-"
+    let muli        : wexpr -> wexpr -> wexpr = _binop _typf_iii "*"
+    let divi        : wexpr -> wexpr -> wexpr = _binop _typf_iii "/"
+    let adds        : wexpr -> wexpr -> wexpr = _binop _typf_sss "+"
+    let subs        : wexpr -> wexpr -> wexpr = _binop _typf_sss "-"
+    let muls        : wexpr -> wexpr -> wexpr = _binop _typf_sss "*"
+    let divs        : wexpr -> wexpr -> wexpr = _binop _typf_sss "/"
+    let addd        : wexpr -> wexpr -> wexpr = _binop _typf_ddd "+"
+    let subd        : wexpr -> wexpr -> wexpr = _binop _typf_ddd "-"
+    let muld        : wexpr -> wexpr -> wexpr = _binop _typf_ddd "*"
+    let divd        : wexpr -> wexpr -> wexpr = _binop _typf_ddd "/"
 
+    let declare     : wtype -> wexpr wlvalue =
+    fun typ ->
+        let nam = _getname () in
+        let decl = typename_decl typ nam in
+        _declare_append decl;
+        (typ, nam)
+    
+    let assign      : wexpr wlvalue -> wexpr -> wstmt =
+    fun (ltyp, lexp) (rtyp, rexp) ->
+        Printf.sprintf "%s=%s;" lexp rexp
+    
+    let struct_elem : wexpr wlvalue -> int -> wexpr wlvalue =
+    fun (styp, sexp) n ->
+        match styp with
+        | Struct(sl) -> 
+            let typ = List.nth sl n in (typ, Printf.sprintf "%s.v%d" sexp n)
+        | _ -> failwith "unexpected data type"
+    
+    let array_elem : wexpr wlvalue -> wexpr -> wexpr wlvalue =
+    fun (styp, sexp) (ntyp, nexp) ->
+        match (styp, ntyp) with
+        | (Array(aet, n), PDT(Int(il))) -> (aet, Printf.sprintf "%s[%s]" sexp nexp)
+        | _ -> failwith "unexpected data type"
 
+    let deref     : 'a wlvalue -> 'a = fun a -> a
 
 
 end
--- plfp/wdata.ml
+++ plfp/wdata.ml
@@ -19,7 +19,7 @@ module WagonDT = struct
     | Vec128d of wpdt (* FDouble *)
     | Vec256d of wpdt (* FDouble *)
     | Vec512d of wpdt (* FDouble *)
-    | Pointer of wpdt (* Pointer of any type *)
+    | Pointer of wtype (* Pointer of any type *)
 
     let wpdt_bitlen : wpdt -> int =
     fun dt ->
@@ -62,7 +62,7 @@ module WagonDT = struct
             end
         | _ -> failwith "unexpected scalar type"
     let v512    : wtype -> wtype = 
-        fun scalar ->
+    fun scalar ->
         match scalar with
         | PDT(dt) -> 
             begin
@@ -75,12 +75,75 @@ module WagonDT = struct
         | _ -> failwith "unexpected scalar type"
     
     let arr     : wtype -> int -> wtype =
-        fun elem n -> Array(elem, n)
+    fun elem n -> Array(elem, n)
     let structure : wtype list -> wtype =
-        fun elem_list -> Struct(elem_list)
+    fun elem_list -> Struct(elem_list)
     let struct_elem : wtype -> int -> wtype =
-        fun str n ->
+    fun str n ->
         match str with
         | Struct(elem_list) -> List.nth elem_list n
+        | _ -> failwith "unexpected data type"
+
+    let _struct_decl : string list ref = ref []
+    let _struct_decl_append : string -> unit =
+    fun decl ->
+        if List.exists (fun s -> s = decl) (!_struct_decl) then ()
+        else _struct_decl := (!_struct_decl) @ [decl]
+    
+    let pdt_name : wpdt -> string =
+    fun pdt -> 
+        match pdt with
+        | Bool -> "bool"
+        | Int(i) -> Printf.sprintf "int%d_t" i
+        | FSingle -> "float"
+        | FDouble -> "double"
+
+    let struct_name : wtype -> string =
+    fun wt ->
+        match wt with
+        | Struct(s) -> Printf.sprintf "ws_%08x" (Hashtbl.hash wt)
+        | _ -> failwith "unexpected data type"
+    let rec typename : wtype -> string =
+    fun wt ->
+        match wt with
+        | PDT(pdt) -> pdt_name pdt
+        | Struct(wtl) -> struct_name wt
+        | Array(it,n) -> Printf.sprintf "%s[%d]" (typename it) n
+        | Vec128i(it) -> "__m128i"
+        | Vec256i(it) -> "__m256i"
+        | Vec512i(it) -> "__m512i"
+        | Vec128s(it) -> "__m128"
+        | Vec256s(it) -> "__m256"
+        | Vec512s(it) -> "__m512"
+        | Vec128d(it) -> "__m128d"
+        | Vec256d(it) -> "__m256d"
+        | Vec512d(it) -> "__m512d"
+        | Pointer(it) -> Printf.sprintf "%s*" (typename it)
+    let typename_decl : wtype -> string -> string =
+    fun wt nam ->
+        match wt with
+        | PDT(pdt)    -> Printf.sprintf "%s %s;" (pdt_name pdt) nam
+        | Struct(wtl) -> Printf.sprintf "struct %s %s;" (struct_name wt) nam
+        | Array(it,n) -> Printf.sprintf "%s %s[%d];" (typename it) nam n
+        | Vec128i(it) -> Printf.sprintf "%s %s;" "__m128i" nam
+        | Vec256i(it) -> Printf.sprintf "%s %s;" "__m256i" nam
+        | Vec512i(it) -> Printf.sprintf "%s %s;" "__m512i" nam
+        | Vec128s(it) -> Printf.sprintf "%s %s;" "__m128" nam
+        | Vec256s(it) -> Printf.sprintf "%s %s;" "__m256" nam
+        | Vec512s(it) -> Printf.sprintf "%s %s;" "__m512" nam
+        | Vec128d(it) -> Printf.sprintf "%s %s;" "__m128d" nam
+        | Vec256d(it) -> Printf.sprintf "%s %s;" "__m256d" nam
+        | Vec512d(it) -> Printf.sprintf "%s %s;" "__m512d" nam
+        | Pointer(it) -> Printf.sprintf "%s* %s;" (typename it) nam
+
+    let struct_decl : wtype -> string =
+    fun pdt ->
+        let rec gen_struct_list ?(id=0) l =
+            match l with 
+            | [] -> ""
+            | hd :: tl -> (Printf.sprintf "\t%s v%d;\n" (typename hd) id) ^ (gen_struct_list tl ~id:(id+1) )
+        in
+        match pdt with
+        | Struct(s) -> Printf.sprintf "struct %s{\n%s};" (struct_name pdt) (gen_struct_list s)
         | _ -> failwith "unexpected data type"
 end
